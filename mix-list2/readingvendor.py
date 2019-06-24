9141e492d0c4eec653228c9945dcc7a3ea53a786
sripaturi1010 <srividya.paturi@hughes.com>
2019-06-24 13:31:56 -0400

    csv code

---
+++ mix-list2/readingvendor.py
@@ -0,0 +1,11 @@
+#!/uxr/bin/env python3
+"""using csv data"""
+import csv
+
+def main():
+    with open("vendor.csv", "r") as vendorfile:
+        vendata = csv.reader(vendorfile, delimiter=",")
+        for row in vendata:
+            print("The IP address " + row[2] +  " requires the driver " + row[3])
+main()
+
