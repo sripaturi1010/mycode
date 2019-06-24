ef4da3536d7c0feda52131b18f735708d048660c
sripaturi1010 <srividya.paturi@hughes.com>
2019-06-24 16:40:54 -0400

    this is the end of the class

---
+++ paramikosftp/sftpmover.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import paramiko
+import os
+
+t = paramiko.Transport("10.10.2.3", 22) ## IP and port
+t.connect(username="bender",password="alta3")
+sftp = paramiko.SFTPClient.from_transport(t)
+
+for x in os.listdir("/home/student/filestocopy/"):
+    if not os.path.isdir("/home/student/filestocopy/"+x):
+        sftp.put("/home/student/filestocopy/"+x,"/tmp/"+x)
+
+sftp.close()
+
